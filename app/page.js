ad76f7642b44b0f25a609fcab61fc6cacf8073f5
Okoli Frank <okolicharles104@gmail.com>
2024-06-21 14:55:50 +0100

    initial setup

---
+++ app/page.js
@@ -0,0 +1,15 @@
+
+
+export default function Index(){
+    const myName ="Joseph";
+
+    return(
+        <div>
+            <h1> My name is{myName}</h1>
+            <p>I am learning ReactJs</p>
+            <p>I am learning ReactJs</p>
+            <p>I am learning ReactJs</p>
+            <p>I am learning ReactJs</p>
+        </div>
+    )
+}
